a7fbb1b9555c9feca15aa35082b5c3a5c6d1f202
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-19 18:38:23 -0600

    Commit - 5 Getting to Know You Answered

--- js/app.js
+++ js/app.js
@@ -1,11 +1,88 @@
+// EASY GOING:
 /* for (let i = 1; i <= 20; i++){
 	console.log(i);
 }
-*/
+
+//GET EVEN:
+
 
 for (let i = 0; i < 200; i += 2) {
 	console.log(i);
 }
+
+
+//EXCITED KITTEN:
+//1.
+
+let i = 1;
+while(i <= 20) {
+	console.log("Love me, pet me! HSSSS!");
+	i++;
+}
+
+
+
+
+//2.
+
+const kittenArray = ["Human why you taking pictures of me?", "The catnip made me do it!", "Why does the red dot always get away?!"];
+
+
+for (let i = 0; i <= 20; i++) {
+	if(i % 2 === 0) {
+	let sayings = kittenArray[Math.floor(Math.random() * 4)];
+	console.log(sayings);
+} else {
+	console.log("Love me, pet me! HSSSS!")
+}
+}
+*/
+/*
+for (let i = 1; i <= 100; i++) {
+	if (i % 3 === 0) {
+		console.log("Fizz");
+	} else if (i % 5 === 0) {
+		console.log("Buzz");
+	} else if (i % 3 && i % 5 === 0) {
+		console.log("FizzBuzz");
+	} else { 
+		console.log(i);
+	}
+}
+*/
+
+const kenny = ["Kenny", 1000, "Austin"];
+
+const jimHaff = ["Jim H", 16, "All cities"];
+
+const reuben = ["Reuben", 22, "Durham"];
+
+const jimClark = ["Jim C", 186, "LA"];
+
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = "Gameboy";
+console.log(kenny);
+
+jimClark[1] = jimClark[1] + 1;
+console.log(jimClark);
+
+ryan[2] = "Gotham City";
+console.log(ryan);
+
+reuben.pop();
+console.log(reuben);
+reuben.push("Chicago");
+console.log(reuben);
+
+jimHaff.pop();
+console.log(jimHaff);
+jimHaff.splice(3, 2, "Redding", "Arvada", "Goshen");
+console.log(jimHaff);
+
+
+
+
 
 
 
