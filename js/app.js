85f957b5a91552344ea9d14eb003d99a6947f3d6
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-19 21:27:00 -0600

    Commit 7 - Kristyn and Thom have their outfits ready for class - array practice

--- js/app.js
+++ js/app.js
@@ -39,18 +39,19 @@ for (let i = 0; i <= 20; i++) {
 */
 /*
 for (let i = 1; i <= 100; i++) {
-	if (i % 3 === 0) {
+	if (i % 3 && i % 5 === 0) {
+		console.log("FizzBuzz");
+	}
+	else if (i % 3 === 0) {
 		console.log("Fizz");
 	} else if (i % 5 === 0) {
 		console.log("Buzz");
-	} else if (i % 3 && i % 5 === 0) {
-		console.log("FizzBuzz");
-	} else { 
+	} 	else { 
 		console.log(i);
 	}
 }
-*/
-/*
+
+
 
 GETTING TO KNOW YOU:
 const kenny = ["Kenny", 1000, "Austin"];
@@ -81,14 +82,70 @@ jimHaff.pop();
 console.log(jimHaff);
 jimHaff.splice(3, 2, "Redding", "Arvada", "Goshen");
 console.log(jimHaff);
-*/
+
 
 //YELL AT NINJA TURTLES
-
+/*
 const ninja = ["donatello", "leonardo", "raphael", "michaelangelo"]; 
 for (let i = 0; i < ninja.length; i++) {
 		console.log(ninja[i].toUpperCase());
 }
+*/
+//RETURN OF THE CLOSETS
+
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+kristynsCloset.shift();
+console.log(kristynsCloset);
+
+const kristynsShoe = "left shoe";
+console.log(kristynsShoe);
+
+//console.log(thomsCloset);
+thomsCloset[2].push(kristynsShoe);  
+console.log(thomsCloset[2]);
+
+for (let i = 0; i < 3; i++) {
+	
+	let outfits = kristynsCloset[Math.floor(Math.random() * 3)];
+	console.log(`Today Kristyn pick out these/this ${outfits}`);
+}
+for ( i = 0; i < 3; i++) {
+	let outfits1 = thomsCloset[Math.floor(Math.random() * 2)];
+	console.log(`Today Thom picked out these/this ${outfits1[i]}`);
+}
+
+
+
 
 
 
